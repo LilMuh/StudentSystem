bec3b6e01de3e165a8e718d1954440c8e73eaef0
Tom Huang <99058458+LilMuh@users.noreply.github.com>
2023-10-13 03:58:57 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,51 @@
+public class Student {
+    // Properties
+    private int id;
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if (id>0){this.id = id;}
+        else{
+            System.out.println("...................................");
+            System.out.println("Student's id must be greater than 0");
+            System.out.println("...................................");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        // Accept 0-year-old and 100-year-old students
+        if(age>=0 && age<=100){this.age = age;}
+        else{
+            System.out.println(".................................");
+            System.out.println("Data for student's age is invalid");
+            System.out.println(".................................");
+        }
+
+    }
+}
---
+++ StudentDataSystem.java
@@ -0,0 +1,104 @@
+public class StudentDataSystem {
+    public static void main(String[] args) {
+        // Create a department (class size: 3)
+        Student[] physics = new Student[3];
+
+        // Create 3 students
+        Student stu1 = new Student(1, "Tom Huang", 23);
+        Student stu2 = new Student(2, "Jessie Lin", 22);
+        Student stu3 = new Student(3, "Luka P", 3);
+
+        // Add students into physics department
+        physics[0]=stu1;
+        physics[1]=stu2;
+        physics[2]=stu3;
+
+        // Solved: Add one more student into the department
+        //         And determine if student id is unique.
+
+        Student stu4 = new Student(4, "Cassie Lin", 18);
+
+        physics = addNewStudent(physics, stu4);
+
+        // Show every student in physics department
+        showAllStudents(physics);
+
+    }
+
+
+    // Show every student in department
+    public static void showAllStudents(Student[] department){
+        System.out.println("----------------------------");
+        for (int i = 0; i < department.length; i++) {
+            System.out.println("Student id: "+department[i].getId());
+            System.out.println("Name: "+department[i].getName());
+            System.out.println("Age: "+department[i].getAge());
+            System.out.println("----------------------------");
+        }
+    }
+
+
+    // Unique Student ID: uniqueId()
+    // Two results:
+    //      1. Unique -> Successfully created student
+    //      2. Not unique -> Try again, the id already exists.
+
+    public static boolean uniqueID(Student[] department, int id){
+        for (int i = 0; i < department.length; i++) {
+            if(id==department[i].getId()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Count how many students stored in the department.
+    public static int countStudents(Student[] department){
+        int count = 0;
+        for (int i = 0; i < department.length; i++) {
+            // If there is "i" position is not empty, student count add 1
+            if(department[i]!=null){count++;}
+        }
+        return count;
+    }
+
+    // Create new student array(department) with length + 1
+    public static Student[] enlargeDepartment(Student[] oldDpm){
+        Student[] newDpm = new Student[oldDpm.length + 1];
+
+        // Copy every student into new department
+        for (int i = 0; i < oldDpm.length; i++) {
+            newDpm[i]=oldDpm[i];
+        }
+        return newDpm;
+    }
+
+    // Add new student into array(department)
+    public static Student[] addNewStudent(Student[] dpm, Student stu){
+        // Unique Student ID: uniqueId()
+        // Two results:
+        //      1. Unique -> Successfully created student
+        //      2. Not unique -> Try again, the id already exists.
+
+        // 1. Unique ID -> Successfully created student
+        if(uniqueID(dpm,stu.getId())){
+            // If department is full, create a new array for this department with length + 1
+            int studentTotal = countStudents(dpm);
+            if (studentTotal == dpm.length) {
+                dpm = enlargeDepartment(dpm);
+                dpm[dpm.length-1]=stu;
+            }
+            // If not full, add student directly (department[count] = stu)
+            else{
+                dpm[studentTotal]=stu;
+            }
+        }
+        // 2. Not unique ID -> Try again, the id already exists.
+        else{
+            System.out.println("Try again, the Student id already exists.");
+        }
+        return dpm;
+    }
+
+
+}
